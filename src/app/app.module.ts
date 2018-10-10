a18f00d4fc15acf27edf2c1a885cab0044f481d1
Mihael Safaric <mihael.safaric@gmail.com>
2018-10-10 21:37:36 +0200

    Implement meetings routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,14 @@
-import { MeetingsContainerModule } from './pages/meetings-container/meetings-container.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { MeetingsContainerComponent } from './pages/meetings-container/meetings-container.component';
-import { MeetingComponent } from './components/meeting/meeting.component';
 
 const routes: Routes = [
   {
     path: '',
     children: [
-      { path: 'meetings', component: MeetingsContainerComponent }
+      { path: 'meetings', loadChildren: './pages/meetings-container/meetings-container.module#MeetingsContainerModule' }
     ]
   }
 ];
@@ -22,7 +19,6 @@ const routes: Routes = [
   ],
   imports: [
     BrowserModule,
-    MeetingsContainerModule,
     RouterModule.forRoot(routes)
   ],
   providers: [],
