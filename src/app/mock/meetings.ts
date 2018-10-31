a4785b4717b0e73248ef90a79d474d8a8f5927a1
Mihael Safaric <mihael.safaric@gmail.com>
2018-10-31 23:35:01 +0100

    Add more mock meetings

--- src/app/mock/meetings.ts
+++ src/app/mock/meetings.ts
@@ -12,14 +12,38 @@ export const mockMeetings = [
     type: 'meetings',
     attributes: {
       title: 'React meeting',
-      held_at: '2018-05-08T10:29:21.000Z'
+      held_at: '2018-06-08T10:29:21.000Z'
     }
   },
   {
-    id: '2',
+    id: '3',
     type: 'meetings',
     attributes: {
-      title: 'WordPress meeting',
+      title: 'Backbone meeting',
+      held_at: '2018-10-01T22:00:00.000Z'
+    }
+  },
+  {
+    id: '4',
+    type: 'meetings',
+    attributes: {
+      title: 'Ember meeting',
+      held_at: '2018-08-01T22:00:00.000Z'
+    }
+  },
+  {
+    id: '5',
+    type: 'meetings',
+    attributes: {
+      title: 'VueJS meeting',
+      held_at: '2018-06-01T22:00:00.000Z'
+    }
+  },
+  {
+    id: '6',
+    type: 'meetings',
+    attributes: {
+      title: 'NodeJS meeting',
       held_at: '2018-05-01T22:00:00.000Z'
     }
   }
